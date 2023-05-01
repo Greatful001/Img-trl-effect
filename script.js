d20cffb22b2db1b156e585ffe8684b3c83552380
Greatful001 <111737576+Greatful001@users.noreply.github.com>
2023-05-01 14:24:28 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const img1 = document.querySelector(".img-1");
+const img2 = document.querySelector(".img-2");
+const img3 = document.querySelector(".img-3");
+const img4 = document.querySelector(".img-4");
+const img5 = document.querySelector(".img-5");
+
+// function
+
+document.addEventListener("mousemove", e => {
+    img1.setAttribute(
+        "style",
+        "top :" + (e.pageY - 225) + "px; left :" + (e.pageX - 200) + 
+        "px"
+    );
+
+    img2.setAttribute(
+        "style",
+        "top :" + (e.pageY - 225) + "px; left :" + (e.pageX - 200) + 
+        "px"
+    );
+
+    img3.setAttribute(
+        "style",
+        "top :" + (e.pageY - 225) + "px; left :" + (e.pageX - 200) + 
+        "px"
+    );
+
+
+    img4.setAttribute(
+        "style",
+        "top :" + (e.pageY - 225) + "px; left :" + (e.pageX - 200) + 
+        "px"
+    );
+    img4.style.backgroundColor = "green";
+
+    
+    img5.setAttribute(
+        "style",
+        "top :" + (e.pageY - 225) + "px; left :" + (e.pageX - 200) + 
+        "px"
+    );
+});
